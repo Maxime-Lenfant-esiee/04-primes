06f91ef11f194cd3297c2d49f634734a4ca29591
Maxime-Lenfant-esiee <maxime.lenfant@edu.esiee.fr>
2024-11-08 13:37:06 +0000

    Nombres premiers

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
+"""
+programme main
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    Ce programme permet de renvoyer true si le nombre entrer en paramètre est premier 
+    sinon il renvoie false
+    """
 
-    # votre code ici
-
-    pass
+    rac = sqrt(p)
+    racine = int(rac)
+    premier = True
+    if p==1:
+        return False
+    for i in range (2,racine+1):
+        if p%i==0 :
+            premier = False
+            break
+    return premier
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    permet d'appeler la fonction pour tester sur les 100 premiers nombres
+    """
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
-        if isprime(n):
+        if isprime (n):
             print(n, end=", ")
 
     print()
---
+++ primes.py
@@ -0,0 +1,40 @@
+"""
+programme primes
+"""
+from math import sqrt
+
+#### Fonction secondaire
+
+
+def isprime(p):
+    """
+    Ce programme permet de renvoyer true si le nombre entrer en paramètre est premier 
+    sinon il renvoie false
+    """
+    rac = sqrt(p)
+    racine = int(rac)
+    premier = True
+    for i in range(2,racine+1):
+        if p%i==0:
+            premier = False
+            break
+    return premier
+
+#### Fonction principale
+
+
+def main():
+    """
+    permet d'appeler la fonction pour tester sur les 100 premiers nombres
+    """
+    # vos appels à la fonction secondaire ici
+
+    for n in range(100):
+        if isprime(n):
+            print(n, end=", ")
+
+    print()
+
+
+if __name__ == "__main__":
+    main()
